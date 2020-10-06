121b0f4d87d8c1126d383cb67856cfbd85f5cb25
Yura Alekseev <yuraalekseev00@gmail.com>
2020-10-06 19:10:10 +0300

    test

---
+++ users/users.controller.js
@@ -0,0 +1,62 @@
+﻿const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const authorize = require('_middleware/authorize')
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticateSchema, authenticate);
+router.post('/register', registerSchema, register);
+router.get('/', authorize(), getAll);
+router.get('/current', authorize(), getCurrent);
+router.get('/:id', authorize(), getById);
+
+module.exports = router;
+
+function authenticateSchema(req, res, next) {
+    const schema = Joi.object({
+        email: Joi.string().email().max(255).required(),
+        password: Joi.string().min(6).max(255).required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function registerSchema(req, res, next) {
+    const schema = Joi.object({
+        name: Joi.string().min(1).max(255).required(),
+        email: Joi.string().max(255).email().required(),
+        password: Joi.string().min(6).max(255).required(),
+        confirm: Joi.string().min(6).max(255).required(),
+    });
+    validateRequest(req, next, schema);
+}
+
+function register(req, res, next) {
+    console.log(req.body);
+    userService.create(req.body)
+        .then(() => res.json({ message: 'Registration successful' }))
+        .catch(next);
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function getCurrent(req, res, next) {
+    res.json(req.user);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
